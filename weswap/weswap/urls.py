27791cf121618ac3b3ceb27f167e62c936164510
Himakar <nhimakar2000@gmail.com>
2024-09-12 14:19:56 +0530

    Merge remote-tracking branch 'origin/main'

--- weswap/weswap/urls.py
+++ weswap/weswap/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('user', include('user_authorization.urls')),
     path('', include('explore.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
