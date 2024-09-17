d6e53befb8506260ea6bf119b659da36dda4536e
VK22 <kowsic.nerella@gmail.com>
2024-09-18 00:28:40 +0530

    otp verification during signup completed

--- weswap/user_authorization/views.py
+++ weswap/user_authorization/views.py
@@ -1,19 +1,16 @@
 from django.contrib.auth import authenticate, login, password_validation
+from django.db.models import Q
+
+from .otp import signup_otp, send_otp, after_signup
 from django.core.exceptions import ValidationError
+from .decorators import otp_verified_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
 from django.http import JsonResponse
 from django.contrib import messages
-from django.conf import settings
 from django.urls import reverse
-
-from .models import Profile
-import random
 import json
 import re
-
-from weswap.user_authorization.decorators import otp_verified_required
 
 
 def home(request):
@@ -47,54 +44,46 @@ def login_view(request):
     return render(request, 'user_authorization/login.html')
 
 
-def after_signup(user, gender, department, current_year, recipient_list):
-    subject = "Welcome to WeSwap! Your Registration is Complete"
-    message = f"""Dear {user.username},
-
-Congratulations and welcome to WeSwap!
-
-We’re excited to have you on board. Your registration has been successfully completed, and you are now part of the WeSwap community. Here’s a quick overview of what you can do next:
-
-1. Explore and List Products: Start by browsing the available products or list your own items for borrowing.
-2. Manage Your Account: Visit your profile to update your details, set preferences, and manage your listings.
-3. Start Swapping: Connect with other users and start exchanging products effortlessly!
-
-Your Account Details:
-
-- Username: {user.username}
-- Email: {user.email}
-
-Need Help? If you have any questions or need assistance, feel free to reach out to our support team at support@weswap.com or visit our Help Center.
-
-Thank you for choosing WeSwap. We hope you have a great experience and enjoy all the benefits of our platform.
-
-Best regards,
-The WeSwap Team
-"""
-    from_email = settings.EMAIL_HOST_USER
-    send_mail(subject, message, from_email, recipient_list)
-    Profile.objects.create(
-        user=user,
-        gender=gender,
-        department=department,
-        current_year=current_year)
+def user_exists(username='', email=''):
+    if username and not email:
+        return User.objects.filter(Q(username__iexact=username)).exists()
+    elif not username and email:
+        return User.objects.filter(Q(email__iexact=email)).exists()
+    elif not username and not email:
+        raise ValueError("At least one of username or email must be provided")
 
 
 def signup_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        gender = request.POST['gender']
-        department = request.POST['department']
-        current_year = request.POST['current_year']
+        data = json.loads(request.body)
+        action = data.get('action')
+        if action == 'send-otp':
+            email = data.get('email')
+            if user_exists(email=email):
+                return JsonResponse({'success': False, 'message': 'email already in use'})
+            else:
+                if signup_otp(email, request):
+                    return JsonResponse({'success': True, 'message': 'OTP sent successfully!'})
+                else:
+                    return JsonResponse({'success': False, 'message': 'enter valid email'})
 
-        # Create a new user
-        user = User.objects.create_user(username=username, email=email, password=password)
-        recipient_list = [email]
-        after_signup(user, gender, department, current_year, recipient_list)
+        elif action == 'verify-otp':
+            if data.get('otp') == request.session.get('otp'):
+                return JsonResponse({'success': True, 'message': 'OTP verified successfully!'})
+            else:
+                return JsonResponse({'success': False, 'message': 'invalid OTP!'})
 
-        return render(request, 'user_authorization/signup_success.html')
+        elif action == 'signup':
+            # Create a new user
+            if user_exists(username=data.get('username')):
+                return JsonResponse({'success': False, 'message': 'Username already in use'})
+            else:
+                user = User.objects.create_user(username=data.get('username'), email=data.get('email'),
+                                                password=data.get('password'))
+                recipient_list = [data.get('email')]
+                after_signup(user, data.get('gender'), data.get('department'), data.get('current_year'), recipient_list)
+                return JsonResponse(
+                    {'success': True, 'message': 'Profile created successfully!', 'redirect': reverse('login')})
 
     return render(request, 'user_authorization/signup.html')
 
@@ -169,36 +158,3 @@ def change_password(request):
             except User.DoesNotExist:
                 return JsonResponse({'success': False, 'message': 'Failed to change password'})
     return render(request, 'user_authorization/change_password.html')
-
-
-def generate_otp():
-    return str(random.randint(10000, 99999))
-
-
-def send_otp(user_otp, email, request):
-    otp = generate_otp()
-    request.session['otp'] = otp
-    subject = 'Your WeSwap OTP for Email Verification'
-    message = f"""
-        Dear {user_otp.username},
-
-        Thank you for registering with WeSwap!
-
-        To complete your registration and verify your email address, please use the following One-Time Password (OTP):
-
-        Your OTP Code: {otp}
-
-        Instructions:
-        Enter the OTP code on the verification page of the WeSwap application.
-        The OTP is valid for 15 minutes. If you did not request this OTP, please disregard this email.
-
-        Need Assistance?
-        If you have any questions or need help, feel free to contact our support team at support@weswap.com or visit our Help Center.
-
-        Thank you for joining WeSwap. We’re excited to have you as part of our community!
-
-        Best regards,
-        The WeSwap Team
-        """
-    from_email = settings.EMAIL_HOST_USER
-    return send_mail(subject, message, from_email, [email])
